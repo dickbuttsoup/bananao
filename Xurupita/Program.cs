12b2ef992318eaa7347420340357383c6543ad15
dickbuttsoup <kriuxo@gmail.com>
2020-01-23 15:44:22 -0300

    simbora

---
+++ Xurupita/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Xurupita {
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+
+}
